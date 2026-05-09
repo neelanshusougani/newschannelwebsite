bbdac0a79f37f76c80c68f08f8fc5f128a7b6ebb
neelanshusougani <neelanshujain35@gmail.com>
2026-05-09 16:17:50 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-module.exports = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
