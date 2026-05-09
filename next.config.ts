a0f60338a9ee8ca0080c472bed0369b8f9c6254e
neelanshusougani <neelanshujain35@gmail.com>
2026-05-09 16:15:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,3 +10,14 @@ module.exports = {
     ignoreBuildErrors: true,
   },
 }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // TypeScript errors ko ignore karega
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ESLint errors ko ignore karega
+  },
+}
+
+module.exports = nextConfig
